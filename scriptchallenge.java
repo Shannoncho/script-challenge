425db3fd0cd3ee4613ad0747b10b89dc45d2812f
Shannoncho <170471@hkis.edu.hk>
2016-08-30 12:18:13 +0800

    init

---
+++ scriptchallenge.java
@@ -0,0 +1,231 @@
+/**
+ * This is the script challenge
+ * 
+ * @author Shannon Cho 
+ * @version version 1.1 August 28
+ */
+
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+import java.io.IOException;
+ public class scriptchallenge {  
+    public static void main(String args[]) { 
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter your name：");
+        String name=in.next();
+        System.out.println("Your name is：\n" + name );  
+        System.out.println("Goodbye : " );      
+
+        String goodbye1 = "Dave. Open the pod bay doors, please, Hal...Open the pod bay doors, please, Hal...Hullo, Hal, do you read me?...Hullo, Hal, do you read me?...Do you read me, Hal?...Do you read me, Hal?...Hullo, Hal, do you read me?...Hullo, Hal, do you read me?...Do you read me, Hal?";
+        String goodbye2 = "Hal. Affirmative, Dave, I read you.";
+        String goodbye3 = "Dave. Open the pod bay doors, Hal.";
+        String goodbye4 = "Hal. I'm sorry, Dave, I'm afraid I can't do that.";
+        String goodbye5 = "Dave. What's the problem?";
+        String goodbye6 = "Hal. I think you know what the problem is just as well as I do.";
+        String goodbye7 = "Dave. What're you talking about, Hal?";
+        String goodbye8 = "Hal. This mission is too important for me to allow you to jeopardise it.";
+        String goodbye9 = "Dave. I don't know what you're talking about, Hal.";
+        String goodbye10 = "Hal. I know that you and Frank were planning to disconnect me, and I'm afraid that's something I cannot allow to happen.";
+        String goodbye11 = "Dave. Where the hell'd you get that idea, Hal?";
+        String goodbye12 = "Hal. Dave, although you took very thorough precautions in the pod against my hearing you, I could see your lips move.";
+        String goodbye13 = "Dave. Alright, Hal. I'll go in through the emergency airlock.";
+        String goodbye14 = "Hal. Without your space- helmet, Dave, you're going to find that rather difficult.";
+        String goodbye15 = "Dave. Hal, I won't argue with you any more. Open the doors.";
+        String goodbye16 = "Hal. Dave, this conversation can serve no purpose any more. Goodbye.";
+        String goodbye17 = "Dave. Hal? Hal. Hal. Hal! Hal!";    
+
+     try{
+       Thread.sleep(2000);
+  
+    }
+     catch(InterruptedException e){
+     System.out.println("error");
+     }
+
+    String replaceAll1 = goodbye1.replaceAll("Dave", name);
+    System.out.println(replaceAll1);
+    try{
+       Thread.sleep(2000);
+  
+    }
+     catch(InterruptedException e){
+     System.out.println("error");
+     }
+          
+     String replaceAll2 = goodbye2.replaceAll("Dave", name);
+    System.out.println(replaceAll2);
+    try{
+       Thread.sleep(2000);
+  
+    }
+     catch(InterruptedException e){
+     System.out.println("error");
+     }
+     
+     
+     
+      String replaceAll3 = goodbye3.replaceAll("Dave", name);
+    System.out.println(replaceAll3);
+    try{
+       Thread.sleep(2000);
+  
+    }
+     catch(InterruptedException e){
+     System.out.println("error");
+     }
+     
+      String replaceAll4 = goodbye4.replaceAll("Dave", name);
+    System.out.println(replaceAll4);
+    try{
+       Thread.sleep(2000);
+  
+    }
+     catch(InterruptedException e){
+     System.out.println("error");
+     }
+     
+      String replaceAll5 = goodbye5.replaceAll("Dave", name);
+    System.out.println(replaceAll5);
+    try{
+       Thread.sleep(2000);
+  
+    }
+     catch(InterruptedException e){
+     System.out.println("error");
+     }
+     
+      String replaceAll6 = goodbye6.replaceAll("Dave", name);
+    System.out.println(replaceAll6);
+    try{
+       Thread.sleep(2000);
+  
+    }
+     catch(InterruptedException e){
+     System.out.println("error");
+     }
+     
+          
+      String replaceAll7 = goodbye7.replaceAll("Dave", name);
+    System.out.println(replaceAll7);
+    try{
+       Thread.sleep(2000);
+  
+    }
+     catch(InterruptedException e){
+     System.out.println("error");
+     }
+     
+          
+      String replaceAll8 = goodbye8.replaceAll("Dave", name);
+    System.out.println(replaceAll8);
+    try{
+       Thread.sleep(2000);
+  
+    }
+     catch(InterruptedException e){
+     System.out.println("error");
+     }
+     
+          
+      String replaceAll9 = goodbye9.replaceAll("Dave", name);
+    System.out.println(replaceAll9);
+    try{
+       Thread.sleep(2000);
+  
+    }
+     catch(InterruptedException e){
+     System.out.println("error");
+     }
+     
+          
+      String replaceAll10 = goodbye10.replaceAll("Dave", name);
+    System.out.println(replaceAll10);
+    try{
+       Thread.sleep(2000);
+  
+    }
+     catch(InterruptedException e){
+     System.out.println("error");
+     }
+     
+          
+      String replaceAll11 = goodbye11.replaceAll("Dave", name);
+    System.out.println(replaceAll11);
+    try{
+       Thread.sleep(2000);
+  
+    }
+     catch(InterruptedException e){
+     System.out.println("error");
+     }
+     
+          
+      String replaceAll12 = goodbye12.replaceAll("Dave", name);
+    System.out.println(replaceAll12);
+    try{
+       Thread.sleep(2000);
+  
+    }
+     catch(InterruptedException e){
+     System.out.println("error");
+     }
+     
+          
+      String replaceAll13 = goodbye13.replaceAll("Dave", name);
+    System.out.println(replaceAll13);
+    try{
+       Thread.sleep(2000);
+  
+    }
+     catch(InterruptedException e){
+     System.out.println("error");
+     }
+     
+          
+      String replaceAll14 = goodbye14.replaceAll("Dave", name);
+    System.out.println(replaceAll14);
+    try{
+       Thread.sleep(2000);
+  
+    }
+     catch(InterruptedException e){
+     System.out.println("error");
+     }
+     
+          
+      String replaceAll15 = goodbye15.replaceAll("Dave", name);
+    System.out.println(replaceAll15);
+    try{
+       Thread.sleep(2000);
+  
+    }
+     catch(InterruptedException e){
+     System.out.println("error");
+     }
+     
+          
+      String replaceAll16 = goodbye16.replaceAll("Dave", name);
+    System.out.println(replaceAll16);
+    try{
+       Thread.sleep(2000);
+  
+    }
+     catch(InterruptedException e){
+     System.out.println("error");
+     }
+     
+     
+          
+      String replaceAll17 = goodbye17.replaceAll("Dave", name);
+    System.out.println(replaceAll17);
+    try{
+       Thread.sleep(2000);
+  
+    }
+     catch(InterruptedException e){
+     System.out.println("error");
+     }
+     
+ 
+     }
+ }
